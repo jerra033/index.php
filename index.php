afc4e96c508e081f975e1ab08d3f67ca467abdf4
jerra033 <133201124+jerra033@users.noreply.github.com>
2023-05-26 13:35:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+</head>
+<body>
+<h1>deel 1</h1>
+<?php
+$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+
+echo count($myArray)
+?>
+
+<h2>deel2</h2>
+<?php
+$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+ echo "het array heeft ".count($myArray)."elementen";
+
+$myArray[]= 'boot';
+echo "het array heeft ".count($myArray);
+?> 
+
+
+</body>
+</html>
